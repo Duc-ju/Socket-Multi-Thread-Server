cafeb9cf080571a4277866d60a31dfaccaec7a7b
Duc-ju <Trangduc56@gmail.com>
2021-10-14 20:07:43 +0700

    first commit

---
+++ src/server/ServerThread.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author Admin
+ */
+public class ServerThread implements Runnable {
+
+    private Socket socketOfServer;
+    private int clientNumber;
+    private BufferedReader is;
+    private BufferedWriter os;
+    private boolean isClosed;
+
+    public BufferedReader getIs() {
+        return is;
+    }
+
+    public BufferedWriter getOs() {
+        return os;
+    }
+
+    public int getClientNumber() {
+        return clientNumber;
+    }
+
+    public ServerThread(Socket socketOfServer, int clientNumber) {
+        this.socketOfServer = socketOfServer;
+        this.clientNumber = clientNumber;
+        System.out.println("Server thread number " + clientNumber + " Started");
+        isClosed = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Mở luồng vào ra trên Socket tại Server.
+            is = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
+            os = new BufferedWriter(new OutputStreamWriter(socketOfServer.getOutputStream()));
+            System.out.println("Khời động luông mới thành công, ID là: " + clientNumber);
+            write("get-id" + "," + this.clientNumber);
+            Server.serverThreadBus.sendOnlineList();
+
+            String message;
+            while (!isClosed) {
+                message = is.readLine();
+                if (message == null) {
+                    break;
+                }
+                String[] messageSplit = message.split(",");
+                if(messageSplit[0].equals("send-to-global")){
+                    Server.serverThreadBus.mutilCastSend("global-message"+","+"Client "+messageSplit[2]+": "+messageSplit[1]);
+                }
+                if(messageSplit[0].equals("send-to-person")){
+                    Server.serverThreadBus.sendMessageToPersion(Integer.parseInt(messageSplit[3]),"Client "+ messageSplit[2]+" (tới bạn): "+messageSplit[1]);
+                }
+            }
+        } catch (IOException e) {
+            isClosed = true;
+            Server.serverThreadBus.remove(clientNumber);
+            System.out.println(this.clientNumber+" đã thoát");
+            Server.serverThreadBus.sendOnlineList();
+            e.printStackTrace();
+            e.printStackTrace();
+        }
+    }
+    public void write(String message) throws IOException{
+        os.write(message);
+        os.newLine();
+        os.flush();
+    }
+}
---
+++ src/server/ServerThreadBus.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class ServerThreadBus {
+    private List<ServerThread> listServerThreads;
+
+    public List<ServerThread> getListServerThreads() {
+        return listServerThreads;
+    }
+
+    public ServerThreadBus() {
+        listServerThreads = new ArrayList<>();
+    }
+
+    public void add(ServerThread serverThread){
+        listServerThreads.add(serverThread);
+    }
+    
+    public void mutilCastSend(String message){
+        for(ServerThread serverThread : Server.serverThreadBus.getListServerThreads()){
+            try {
+                serverThread.getOs().write(message);
+                serverThread.getOs().newLine();
+                serverThread.getOs().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public int getLength(){
+        return listServerThreads.size();
+    }
+    
+    public void sendOnlineList(){
+        String res = "";
+        List<ServerThread> threadbus = Server.serverThreadBus.getListServerThreads();
+        for(ServerThread serverThread : threadbus){
+            res+=serverThread.getClientNumber()+"-";
+        }
+        for(ServerThread serverThread : threadbus){
+            try {
+                System.out.println(res);
+                serverThread.getOs().write("update-online-list"+","+res);
+                serverThread.getOs().newLine();
+                serverThread.getOs().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public void sendMessageToPersion(int id, String message){
+        for(ServerThread serverThread : Server.serverThreadBus.getListServerThreads()){
+            if(serverThread.getClientNumber()==id){
+                try {
+                    serverThread.write("global-message"+","+message);
+                    break;
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+    public void remove(int id){
+        for(int i=0; i<Server.serverThreadBus.getLength(); i++){
+            if(Server.serverThreadBus.getListServerThreads().get(i).getClientNumber()==id){
+                Server.serverThreadBus.listServerThreads.remove(i);
+            }
+        }
+    }
+}
